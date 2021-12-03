e97440935a82c7ba58f106ad1c06209ef566da77
DannaMont <95336394+DannaMont@users.noreply.github.com>
2021-12-03 16:53:09 -0600

    EMTECH docu

---
+++ ejercicios.py
@@ -0,0 +1,35 @@
+"""
+
+Índices y Slicing
+
+"""
+
+frase = 'Qué frase tan épica!'
+
+que = frase[0:3]
+print(que)
+
+# Imprime la siguiente variable
+# que = frase[:0] 
+
+# epica = frase[-6:]
+# print(epica)
+
+"""
+
+Ejercicios para practicar
+
+"""
+
+estaturas = [160, 175, 166, 150, 180, 170]
+
+# Guarda en dos variables el arreglo, divídela
+# en partes iguales, o desiguales. Experimenta.
+
+# grupoA = estaturas[]
+# grupoB = estaturas[]
+
+# Imprime el último elemento del arreglo
+
+# ult = estaturas[]
+# print()
